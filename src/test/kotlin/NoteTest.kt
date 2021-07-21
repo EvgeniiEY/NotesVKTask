d054b1fb1b15b5478ca2005ca47b9fdc00601df3
Evgenii Yakhontov <yakhontov.e@yandex.ru>
2021-07-22 01:49:20 +0300

    task 3.2 corrections 2

--- src/test/kotlin/NoteTest.kt
+++ src/test/kotlin/NoteTest.kt
@@ -1,10 +1,39 @@
 import org.junit.Test
 
 import org.junit.Assert.*
+import ru.netology.*
+import ru.netology.Note
+
 
 class NoteTest {
 
+
     @Test
-    fun createCom() {
+    fun createComTEST() {
+        val noteTest = Note("Test NOTE", "Text of TEST note")
+        val commentTest = Comment(99, "NICE TEST COMMENT!", 1)
+        val notesMassiveTest = NotesVKTask()
+        val NoteId = 99
+        var CommentId = 1
+// Как сослаться на ф-ю в классе Note?
+
+        val result = Note(createCom)
+
+
+
+
+
+        assertTrue(true)
     }
+
+    @Test
+    fun findNoteTest() {
+        val noteId = 1
+
+        val result = findNote(noteId)
+
+
+    }
+
 }
+
---
+++ src/test/kotlin/NotesVKTaskTest.kt
@@ -0,0 +1,27 @@
+//import junit.framework.Assert.assertEquals
+//import junit.framework.Assert.assertTrue
+//import org.junit.Test
+//import NotesVKTask
+//
+//class NotesVKTaskTest {
+//
+//    val noteTest = Note("Test NOTE", "Text of TEST note")
+//    val commentTest = Comment(99, "NICE TEST COMMENT!", 1)
+//    val notesMassiveTest = NotesVKTask()
+//
+//    @Test
+//    fun test() {
+//        val note1 = Note("TEST_note1", "TEST_note1")
+//        val note2 = Note("TEST_note2", "TEST_note2")
+//        val note3 = Note("TEST_note3", "TEST_note3")
+//
+//        val update = note1.comments.add(Comment(99,"UPDATE COMMENT",1))
+//
+//
+//
+//
+//        assertTrue("UPDATE COMMENT",true)
+//    }
+//
+//
+//}
